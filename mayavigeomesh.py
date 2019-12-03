1fbc276623b042d201ecb893cc70449dd50f10e0
Liam Kilcommons <liamk@mathias>
2019-12-03 11:39:13 -0700

    Refactor for clarity, rename a few things

--- mayavigeomesh.py
+++ mayavigeomesh.py
@@ -101,18 +101,79 @@ def solar_zenith_angle(dt,lats,lons,degrees=True):
     else:
         return np.arccos(cossza)
 
-def manual_sphere(image_file):
-    # caveat 1: flip the input image along its first axis
-    img = plt.imread(image_file) # shape (N,M,3), flip along first dim
+def download_blue_marble():
+    """from https://visibleearth.nasa.gov/collection/1484/blue-marble"""
+    imgfn = os.path.join(tempfile.gettempdir(),'blue_marble.jpg')
+    if not os.path.exists(imgfn):
+        print('Downloading NASA blue marble image...')
+        url = ('https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73909/'
+              +'world.topo.bathy.200412.3x5400x2700.jpg')
+        r = requests.get(url, allow_redirects=True)
+        open(imgfn,'wb').write(r.content)
+    return imgfn
+
+def get_blue_marble_texture_imgfn():
+    """Flips the image inside-out so that it has the right
+    'chirality' for use as a texture map in Mayavi, and also
+    divides the image in half at the (approximate) prime meridian, exchanging
+    the two halves of the image so that the prime meridan is at the edges of the
+    image, which means Mayavi will wrap it around the sphere such that the 
+    X axis of the figure is aligned with the prime meridian, and Z axis
+    is in the direction of the North pole. This makes figure
+    coordinates a rough appoximation of 
+    Earth-Centered Earth-Fixed coordinates
+    """
+    imgfn = download_blue_marble() # shape (N,M,3), flip along first dim
     outfile = image_file.replace('.jpg', '_flipped.jpg')
     # flip output along first dim to get right chirality of the mapping
-    img = img[::-1,...]
+    flipped_img = img.copy()
+    flipped_img = img[::-1,...]
+
     ny = img.shape[1]
     recentered_img = img.copy()
     recentered_img[:,:ny/2,:]=img[:,ny/2:,:]
     recentered_img[:,ny/2:,:]=img[:,:ny/2,:]
+    
     plt.imsave(outfile, recentered_img)
-    image_file = outfile  # work with the flipped file from now on
+    return outfile
+
+def map_image_to_unit_sphere_actor(image_file):
+    """This is a more low-level way of accomplishing a unit sphere
+    textured with an image in Mayavi. Prefer image_textured_unit_sphere
+    because it generates a mlab object which can be manipulated through
+    the Mayavi GUI
+    This code is largely borrowed from:
+    https://stackoverflow.com/questions/53074908/map-an-image-onto-a-sphere-and-plot-3d-trajectories
+    """
+
+    # load and map the texture
+    img = tvtk.JPEGReader()
+    img.file_name = image_file
+    texture = tvtk.Texture(input_connection=img.output_port) #, interpolate=1
+    # (interpolate for a less raster appearance when zoomed in)
+
+    # use a TexturedSphereSource, a.k.a. getting our hands dirty
+    R = 1
+    Nrad = 180
+
+    # create the sphere source with a given radius and angular resolution
+    sphere = tvtk.TexturedSphereSource(radius=R, theta_resolution=Nrad,
+                                       phi_resolution=Nrad)
+
+    # assemble rest of the pipeline, assign texture    
+    sphere_mapper = tvtk.PolyDataMapper(input_connection=sphere.output_port)
+    sphere_actor = tvtk.Actor(mapper=sphere_mapper, texture=texture)
+    return sphere_actor
+
+def image_textured_unit_sphere_mesh(image_file):
+    """Generates a spherical Mayavi mesh and overlays an jpg image
+    as a texture. The image is assumed to be a rectangular map in an
+    projection which maps appropriately onto the meshace of a sphere
+    without distortion (e.g. the NASA Blue Marble image). The image
+    is also assumed to completely cover the sphere without repeats or overlap
+    This code is largely borrowed from:
+    https://stackoverflow.com/questions/53074908/map-an-image-onto-a-sphere-and-plot-3d-trajectories
+    """
 
     # parameters for the sphere
     R = 1 # radius of the sphere
@@ -142,46 +203,55 @@ def manual_sphere(image_file):
     # caveat 2: if prevent_seam is 1 (default), half the image is used to map half the sphere
     cylinder_mapper.prevent_seam = 0 # use 360 degrees, might cause seam but no fake data
     #cylinder_mapper.center = np.array([0,0,0])  # set non-trivial center for the mapping sphere if necessary
+    return mesh
 
-def map_image_to_unit_sphere_actor(image_file):
-    # load and map the texture
-    img = tvtk.JPEGReader()
-    img.file_name = image_file
-    texture = tvtk.Texture(input_connection=img.output_port) #, interpolate=1
-    # (interpolate for a less raster appearance when zoomed in)
+def blue_marble_unit_sphere_mesh():
+    """Generates a unit-sphere mayavi mesh textured with 
+    a NASA blue marble map
+    """
+    return image_textured_unit_mesh(get_blue_marble_texture_imgfn())
 
-    # use a TexturedSphereSource, a.k.a. getting our hands dirty
-    R = 1
-    Nrad = 180
-
-    # create the sphere source with a given radius and angular resolution
-    sphere = tvtk.TexturedSphereSource(radius=R, theta_resolution=Nrad,
-                                       phi_resolution=Nrad)
-
-    # assemble rest of the pipeline, assign texture    
-    sphere_mapper = tvtk.PolyDataMapper(input_connection=sphere.output_port)
-    sphere_actor = tvtk.Actor(mapper=sphere_mapper, texture=texture)
-    return sphere_actor
-
-def lut_set_transparent_below_range(lut):
+def lut_set_out_of_range_transparent(lut,above=False,below=False):
     """Set data below vmin to a transparent color"""
-    lut.use_below_range_color=1
-    lut.below_range_color=np.array([0.,0.,0.,0.])
+    if not above and not below:
+        raise ValueError(('Out of range transparency called but above or below'
+                            'kwargs not specified.'))
+    if below:
+        lut.use_below_range_color=1
+        lut.below_range_color=np.array([0.,0.,0.,0.])
+    if above:
+        lut.use_above_range_color=1
+        lut.above_range_color=np.array([0.,0.,0.,0.])
 
-def lut_set_alpha_range(lut,amin=0.,amax=1.):
-    """following
+def lut_set_alpha_range(lut,amin=0.,amax=1.,diverging=False):
+    """
+    Modify a colormap (look up table, lut in Mayavi language)
+    have a transparency gradient.
+
+    'diverging' boolean kwarg will make the transparency minimum at the
+    middle of the colormap and maximum at the ends
+
+    See also:
     https://docs.enthought.com/mayavi/mayavi/auto/example_custom_colormap.html
-    with option of range of alphas other than 0 to 1
     """
     lut_table = lut.table.to_array()
     n_colors = lut_table.shape[0]
     # docs say look up table (lut.table) needs integers
     iamin = np.floor(255*amin)
     iamax = np.floor(255*amax)
-    lut_table[:,-1] = np.floor(np.linspace(iamin,iamax,n_colors))
+    if not diverging:
+        alphas = np.floor(np.linspace(iamin,iamax,n_colors))
+    else:
+        half_range_alphas = np.floor(np.linspace(iamin,iamax,n_colors/2))
+        alphas = np.concatenate([half_range_alphas.flip(),half_range_alphas]) 
+        
+    lut_table[:,-1] = alphas
     lut.table = lut_table
 
 def legend(scalar_lut_manager,label=None):
+    """
+    Show a colorbar
+    """
     scalar_lut_manager.show_scalar_bar = True
     scalar_lut_manager.show_legend = True
     scalar_lut_manager.data_name = label if label is not None else u''
@@ -196,17 +266,24 @@ def geo2cart(glats,glons,alt=110.):
     z = r*np.cos(th)
     return x,y,z
 
-def update_geosurf(mesh,glats,glons,geodata,alt=300.):
+def update_geomesh(mesh,glats,glons,geodata,alt=300.):
     x,y,z = geo2cart(glats,glons,alt)
     mesh.mlab_source.trait_set(x=x,y=y,z=z,scalars=geodata)
     mlab.draw()
 
-def geosurf(glats,glons,geodata,alt=300.,
-                vmin=None,vmax=None,cmap='viridis',reverse_cmap=False,
-                amin=None,amax=None,alpha=None,
+def geomesh(glats,glons,geodata,alt=300.,
+                vmin=None,vmax=None,cmap='viridis',
+                amin=None,amax=None,diverging_tranparency=False,alpha=None,
                 colorbar=False,label=None,
                 transparent_below_range=False):
-    """Draw gridded data in geographic coordinates as a surface (mesh) """
+    """Draw gridded data in geographic coordinates as a meshace (mesh) """
+
+    if cmap[-2:] == '_r':
+        reverse_cmap = True
+        cmap = cmap.replace('_r','')
+    else:
+        reverse_cmap = False
+
     x,y,z = geo2cart(glats,glons,alt)
     mesh = mlab.mesh(x,y,z,
                         scalars=geodata,
@@ -214,18 +291,21 @@ def geosurf(glats,glons,geodata,alt=300.,
                         vmax=vmax,
                         colormap=cmap)
     
+    mesh.parent.scalar_lut_manager.reverse_lut = reverse_cmap
+
     if transparent_below_range:
         lut_set_transparent_below_range(mesh.parent.scalar_lut_manager.lut)
     
     if amin is not None and amax is not None:
         lut_set_alpha_range(mesh.parent.scalar_lut_manager.lut,
-                                                amin=amin,amax=amax)
-
-    mesh.parent.scalar_lut_manager.reverse_lut = reverse_cmap 
+                                                amin=amin,amax=amax,
+                                                diverging=diverging_tranparency)
     if colorbar:
         legend(mesh.parent.scalar_lut_manager,label=label)
+
     if alpha is not None:
         mesh.parent.scalar_lut_manager.lut.alpha=alpha
+
     mlab.draw()
     return mesh
 
@@ -278,31 +358,20 @@ def calculate_dark(dt):
     dark = (sza-90.)/90.+1.
     return glats,glons,dark
 
-def darkness_geosurf(dt):
-    """Draw a surface over the dark portion of the planet,
+def darkness_geomesh(dt):
+    """Draw a meshace over the dark portion of the planet,
     i.e. regions with solar zenith angle > 90
     """
     glats,glons,dark = calculate_dark(dt)
     #gist_yarg is a white to black colormap
-    mesh = geosurf(glats,glons,dark,alt=50.,
+    mesh = geomesh(glats,glons,dark,alt=50.,
                     vmin=1.,vmax=1.01,alpha=.2,transparent_below_range=True,
-                    cmap='bone',reverse_cmap=True)
+                    cmap='bone_r')
     lut = mesh.parent.scalar_lut_manager.lut
     lut.use_above_range_color=1
     lut.above_range_color=np.array([0.,0.,0.,.75])
     mlab.draw()
     return mesh
-
-def download_blue_marble():
-    """from https://visibleearth.nasa.gov/collection/1484/blue-marble"""
-    imgfn = os.path.join(tempfile.gettempdir(),'blue_marble.jpg')
-    if not os.path.exists(imgfn):
-        print('Downloading NASA blue marble image...')
-        url = ('https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73909/'
-              +'world.topo.bathy.200412.3x5400x2700.jpg')
-        r = requests.get(url, allow_redirects=True)
-        open(imgfn,'wb').write(r.content)
-    return imgfn
 
 def download_auroral_nowcast():
     nowcastfn = os.path.join(tempfile.gettempdir(),'auroral_nowcast.txt')
@@ -317,45 +386,25 @@ def nowcast_auroral_probability():
     aur_prob = np.genfromtxt(nowcasttxtfn).reshape((512,1024))
     return glats,glons,aur_prob
 
-def visual_test_update_geosurf():
-    """Make a plot of the terminator and sync the camera to
-    the solar position to mimic earth rotation
-    """
-    fig = mlab.figure(size=(600, 600))
-    
-    imagefn = download_blue_marble()
-    manual_sphere(imagefn)
-
-    dt = datetime.datetime.utcnow()
-    dark_mesh = darkness_geosurf(dt)
-    set_camera_position(5.,0.,5.)
-
-    for i in range(24):
-        dt = datetime.datetime(2019,11,30,i)
-        glats,glons,dark = calculate_dark(dt)
-        camera_to_sun_sync(dt)
-        update_geosurf(dark_mesh,glats,glons,dark,alt=50.)
-        mlab.savefig('earth_{}.png'.format(i))
-
-def visual_test_geosurf():
+def visual_test_geomesh():
     """Make a plot of the current NOAA auroral probability on the NASA
     blue marble image
     """
-    imagefn = download_blue_marble()
     
     # create a figure window (and scene)
     fig = mlab.figure(size=(600, 600))
     #fig.scene.scene.background_color=[0,0,0]
     #sphere_actor = map_image_to_unit_sphere_actor(imagefn)
     #fig.scene.add_actor(sphere_actor)
-    manual_sphere(imagefn)
+ 
+    earth_mesh = blue_marble_unit_sphere_mesh()
 
     dt = datetime.datetime.utcnow()
-    dark_mesh = darkness_geosurf(dt)
+    dark_mesh = darkness_geomesh(dt)
     camera_to_sun_sync(dt,distance=3.)
 
     glats,glons,aur_prob = nowcast_auroral_probability()
-    aur_mesh = geosurf(glats,glons,aur_prob,
+    aur_mesh = geomesh(glats,glons,aur_prob,
                 label='Probability of Aurora',
                 cmap='viridis',vmin=1.,vmax=15.,amin=.5,amax=1.,
                 colorbar=True,transparent_below_range=True)
@@ -363,10 +412,31 @@ def visual_test_geosurf():
     ttext = mlab.text(.05,.05,dt.strftime('%m/%d/%y %H:%M'),line_width=.75)
     ttext = mlab.text(.05,.02,'NOAA SWPC Aurora Nowcast',line_width=.75)
 
-    mlab.savefig('geosurf_test.png')
+    mlab.savefig('geomesh_test.png')
+
+    mlab.show()
+
+def visual_test_update_geomesh():
+    """Make a plot of the terminator and sync the camera to
+    the solar position to mimic earth rotation
+    """
+    fig = mlab.figure(size=(600, 600))
+    
+    earth_mesh = blue_marble_unit_sphere_mesh()
+
+    dt = datetime.datetime.utcnow()
+    dark_mesh = darkness_geomesh(dt)
+    set_camera_position(5.,0.,5.)
+
+    for i in range(24):
+        dt = datetime.datetime(2019,11,30,i)
+        glats,glons,dark = calculate_dark(dt)
+        camera_to_sun_sync(dt)
+        update_geomesh(dark_mesh,glats,glons,dark,alt=50.)
+        mlab.savefig('earth_{}.png'.format(i))
 
     mlab.show()
 
 if __name__ == "__main__":
-    visual_test_geosurf()
-    #visual_test_update_geosurf()
+    visual_test_geomesh()
+    #visual_test_update_geomesh()
